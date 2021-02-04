4e420b024c006fab18c86978ba29c13a0b722f7a
simonj-entry <78244639+simonj-entry@users.noreply.github.com>
2021-02-04 16:26:56 +0900

    Update StrongBlock.js

--- StrongBlock.js
+++ StrongBlock.js
@@ -498,7 +498,7 @@ const LibraryCreator = {
       class: 'text',
       func: async (sprite, script) => { // 실행할 JS 코드
         // script.getValue('위에 map에서 설정한 변수 이름', script) 이 코드로 입력값 로드 가능
-        open('script.getValue('SEARCHRESULT', script));
+        open(script.getValue('SEARCHRESULT', script));
         return script.callReturn() // 일반 블럭 코드 뒤에는 반드시 붙여주세요
       },
     },
